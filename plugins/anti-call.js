d5387ce802527ea20973bb30d2c1bc19868ba24a
Frfjhfghv <margarettedessources42@gmail.com>
2025-06-25 08:42:20 -0400

    Update anti-call.js

--- plugins/anti-call.js
+++ plugins/anti-call.js
@@ -15,12 +15,12 @@ cmd({
 
   if (text.toLowerCase() === "on") {
     antiCallEnabled = true;
-    m.reply("*✅ Anti-Call enabled.*");
+    m.reply("*✅ ᴀɴᴛɪ-ᴄᴀʟʟ ᴇɴᴀʙʟᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.*");
   } else if (text.toLowerCase() === "off") {
     antiCallEnabled = false;
-    m.reply("*❌ Anti-Call disabled.*");
+    m.reply("*❌ ᴀɴᴛɪ-ᴄᴀʟʟ ᴅɪsᴀʙʟᴇᴅ.*");
   } else {
-    m.reply("*Use:* .anticall on | off");
+    m.reply("*Use:* .ᴀɴᴛɪᴄᴀʟʟ ᴏɴ | ᴏғғ");
   }
 });
 
@@ -38,7 +38,7 @@ cmd({ on: "body" }, async (conn, m, msg, { from }) => {
 
           if (!call.isGroup) {
             await conn.sendMessage(call.from, {
-              text: "*📵 Call automatically rejected. The owner is currently busy.*",
+              text: "*📵 ᴄᴀʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʀᴇᴊᴇᴄᴛᴇᴅ. The ᴏᴡɴᴇʀ ɪs ᴄᴜʀʀᴇɴᴛʟʏ ʙᴜsʏ.*",
               mentions: [call.from]
             });
           }
