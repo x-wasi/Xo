7d0e3886a955da7af62a855fd1be6de52ffb0305
Frfjhfghv <margarettedessources42@gmail.com>
2025-06-29 00:08:01 -0400

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -2,45 +2,9 @@ const { fetchJson } = require("../lib/functions");
 const { downloadTiktok } = require("@mrnima/tiktok-downloader");
 const { facebook } = require("@mrnima/facebook-downloader");
 const cheerio = require("cheerio");
-const { igdl } = require("ruhend-scraper");
 const axios = require("axios");
 const { cmd, commands } = require('../command');
 
-cmd({
-  pattern: "ig",
-  alias: ["insta", "Instagram"],
-  desc: "To download Instagram videos.",
-  react: "🎥",
-  category: "download",
-  filename: __filename
-}, async (conn, m, store, { from, q, reply }) => {
-  try {
-    if (!q || !q.startsWith("http")) {
-      return reply("❌ Please provide a valid Instagram link.");
-    }
-
-    await conn.sendMessage(from, {
-      react: { text: "⏳", key: m.key }
-    });
-
-    const response = await axios.get(`https://api.davidcyriltech.my.id/instagram?url=${q}`);
-    const data = response.data;
-
-    if (!data || data.status !== 200 || !data.downloadUrl) {
-      return reply("⚠️ Failed to fetch Instagram video. Please check the link and try again.");
-    }
-
-    await conn.sendMessage(from, {
-      video: { url: data.downloadUrl },
-      mimetype: "video/mp4",
-      caption: "📥 *Instagram Video Downloaded Successfully!*"
-    }, { quoted: m });
-
-  } catch (error) {
-    console.error("Error:", error);
-    reply("❌ An error occurred while processing your request. Please try again.");
-  }
-});
 
 
 // twitter-dl
