7d2360190773c6db65dcf32f6ad2c0d5d0661063
Frfjhfghv <margarettedessources42@gmail.com>
2025-06-29 15:42:19 -0400

    Delete models directory

--- models/Notice.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const noticeSchema = new mongoose.Schema({
-  message: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Notice", noticeSchema);
--- models/plugins.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const pluginSchema = new mongoose.Schema({
-    id: String,
-    url: String,
-    installedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Plugin', pluginSchema);
