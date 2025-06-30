647f388f40e81aa20cdda29d826599ee0d0da294
Frfjhfghv <margarettedessources42@gmail.com>
2025-06-30 03:18:58 -0400

    Update ytdl-fast.js

--- plugins/ytdl-fast.js
+++ plugins/ytdl-fast.js
@@ -1,8 +1,7 @@
 const config = require('../config');
 const { cmd } = require('../command');
 const { ytsearch } = require('@dark-yasiya/yt-dl.js');
-const fetch = require('node-fetch');
-const ytsearch = require('yt-search');
+
 
 // MP4 video download
 
@@ -54,43 +53,5 @@ cmd({
     } catch (e) {
         console.log(e);
         reply("An error occurred. Please try again later.");
-    }
-});
-// MP3 song download 
-
-
-
-
-cmd({ 
-    pattern: "play", 
-    alias: ["song02", "mp3"], 
-    react: "🎶", 
-    desc: "Download YouTube song", 
-    category: "download", 
-    use: '.song <query>', 
-    filename: __filename 
-}, async (conn, mek, m, { from, sender, reply, q }) => { 
-    try {
-        if (!q) return reply("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ sᴏɴɢ ɴᴀᴍᴇ ᴏʀ ʏᴏᴜᴛᴜʙᴇ ʟɪɴᴋ.");
-
-        const yt = await ytsearch(q);
-        if (!yt.results.length) return reply("No results found!");
-
-        const song = yt.results[0];
-        const apiUrl = `https://apis.davidcyriltech.my.id/download/ytmp3?url=${encodeURIComponent(song.url)}`;
-        
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-
-        if (!data?.result?.downloadUrl) return reply("Download failed. Try again later.");
-
-    await conn.sendMessage(from, {
-    audio: { url: data.result.downloadUrl },
-    mimetype: "audio/mpeg",
-    fileName: `${song.title}.mp3`}, { quoted: mek });
-
-    } catch (error) {
-        console.error(error);
-        reply("An error occurred. Please try again.");
     }
 });
