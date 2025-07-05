be7fc0ca0599c36127a1e907079ca22366fe4927
Frfjhfghv <margarettedessources42@gmail.com>
2025-07-05 18:06:27 -0400

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -1,5 +1,7 @@
 const config = require('../config');
 const { cmd, commands } = require('../command');
+const { runtime } = require('../lib/functions');
+const axios = require('axios');
 
 cmd({
   pattern: "menu2",
