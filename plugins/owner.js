8054324e298e964258d48897cfa62e1224ddcdea
Frfjhfghv <margarettedessources42@gmail.com>
2025-07-05 15:08:17 -0400

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -41,7 +41,7 @@ END:VCARD
 > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅʏʙʏ ᴛᴇᴄʜ*`;
 
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/2ozipw.jpg' },
+            image: { url: 'https://files.catbox.moe/roubzi.jpg' },
             caption,
             contextInfo: {
                 mentionedJid: [`${ownerNumber.replace('+', '')}@s.whatsapp.net`], 
