d6edebb184e6ee134ad4d2e07ebf168451e33f42
Frfjhfghv <margarettedessources42@gmail.com>
2025-07-15 13:29:07 -0400

    Update sticker-maker.js

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from, pushname }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = `${pushname}`,
+        let pack = `${pushname}`; // ← CORRIGÉ ici
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
